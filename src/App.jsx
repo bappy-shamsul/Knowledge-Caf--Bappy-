087d08b120594491d6a6b298e74ef00533414e17
Yusha <faizan.mashuq@gmail.com>
2024-09-22 20:49:16 +0600

    Heading is modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-7xl text-fuchsia-950 text bg-gray-300'>Knowledge Cafe Bappy</h1>
+      <h1 className='text-7xl text-fuchsia-950 text bg-gray-300'>Knowledge Cafe Banagladesh</h1>
 
     </>
   )

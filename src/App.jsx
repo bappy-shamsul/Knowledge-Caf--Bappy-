229153312f0adb9941d64bc112d32b65404083ce
Yusha <faizan.mashuq@gmail.com>
2024-09-22 20:47:46 +0600

    Tailwind is added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl text-fuchsia-950 text bg-gray-300'>Knowledge Cafe Bappy</h1>
+
+    </>
+  )
+}
+
+export default App

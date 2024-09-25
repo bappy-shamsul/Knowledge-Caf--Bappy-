3257d617a807cacd658dfecab7a3cc9447e5af8e
Yusha <faizan.mashuq@gmail.com>
2024-09-25 23:22:16 +0600

    Bookmark BG is added

--- src/Bookmark/Bookmark.jsx
+++ src/Bookmark/Bookmark.jsx
@@ -6,8 +6,8 @@ import PropTypes from 'prop-types';
 function Bookmark({bookmark}) {
     const {title} = bookmark
     return (
-        <div>
-            <h4>{title}</h4>
+        <div className='bg-sky-200 m-4 p-4'>
+            <h4 className='text-2xl text-black-600'>{title}</h4>
         </div>
     );
 }
